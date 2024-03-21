96c78bb2582314a0e708500ee4127b4304c929d8
Cauã Ghiraldi Silva <caua.ligin@gmail.com>
2024-03-21 02:06:01 -0400

    graças a Deus

--- Exercícios arquivos/exercicioarquivo2.py
+++ Exercícios arquivos/exercicioarquivo2.py
@@ -17,20 +17,19 @@ for i in range(0, len(notas), 3):
     media = sum(sublista) / len(sublista)
     medias.append(media)
 nomes_alunos = nomes[::4]
-for i in range(len(nomes_alunos)):
-    nome = nomes_alunos[i]
-    medias_alunos = medias[i]
-    if medias_alunos >= 7:
-        with open ("aprovados.txt", "w") as arquivo_aprovados:
-                arquivo_aprovados.write(f"{nome}, media de:{medias_alunos}\n")
-    if medias_alunos >=5:
-        if medias_alunos < 7:
-            with open ("exame.txt", "w") as arquivo_exame:
-                arquivo_exame.write(f"{nome}, media de:{medias_alunos}\n")
-    if medias_alunos < 5:
-        with open ("reprovados.txt", "w") as arquivo_reprovados:
+with open("aprovados.txt", "w") as arquivo_aprovados, \
+     open("exame.txt", "w") as arquivo_exame, \
+     open("reprovados.txt", "w") as arquivo_reprovados:
+    for i in range(len(nomes_alunos)):
+        nome = nomes_alunos[i]
+        medias_alunos = medias[i]
+        if medias_alunos >= 7:
+            arquivo_aprovados.write(f"{nome}, media de:{medias_alunos}\n")
+        elif 7 > medias_alunos >= 5:
+            arquivo_exame.write(f"{nome}, media de:{medias_alunos}\n")
+        else:
             arquivo_reprovados.write(f"{nome}, media de:{medias_alunos}\n")
-    print(f"{nome}, {medias_alunos}")
+        print(f"{nome}, {medias_alunos}")
 
 print(notas)
 print(medias)
