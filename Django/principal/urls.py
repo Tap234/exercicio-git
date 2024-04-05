7e4dd1b115966acae6d94fbb9d34077877a1b3d1
Tap234 <caua.ligin@gmail.com>
2024-04-04 23:09:20 -0300

    django app

--- Django/principal/urls.py
+++ Django/principal/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for OlaMundo project.
+URL configuration for principal project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('helloword/', include('helloword.urls')),
     path('admin/', admin.site.urls),
 ]
