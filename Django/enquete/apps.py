456be2e799ee70ca57a2793bf57328f5b8da221c
Tap234 <caua.ligin@gmail.com>
2024-04-11 21:18:53 -0300

    django

--- Django/enquete/apps.py
+++ Django/enquete/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HellowordConfig(AppConfig):
+class enqueteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'helloword'
+    name = 'enquete'
